8a0d1f545ec6eac3dcfae5b03849f9e1a2c4de11
Marcos Castro <mpcc.show@gmail.com>
2023-08-03 08:50:49 -0300

    main view initial setup

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,8 +1,43 @@
 <script setup>
+import { ref } from 'vue'
+import TabView from 'primevue/tabview'
+import TabPanel from 'primevue/tabpanel'
 import Button from 'primevue/button'
+const tabs = ref([
+  {
+    title: 'Tab 1',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  },
+  {
+    title: 'Tab 2',
+    content:
+      'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi      architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.'
+  },
+  {
+    title: 'Tab 3',
+    content:
+      'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.'
+  }
+])
 </script>
 
 <template>
-  <h1><i class="fa-solid fa-folder" /> Hello World</h1>
-  <Button label="Submit" />
+  <TabView :scrollable="true">
+    <TabPanel v-for="tab in tabs" :key="tab.title" :header="tab.title">
+      <template #header="slotProps">
+        <span>{{ slotProps.title }}</span>
+        <Button
+          icon="fa-solid fa-xmark"
+          class="p-0 ml-2 w-1rem h-1rem"
+          size="small"
+          severity="secondary"
+          text
+          rounded
+          aria-label="Close tab"
+        />
+      </template>
+      <p>{{ tab.content }}</p>
+    </TabPanel>
+  </TabView>
 </template>
